32cfa8ac2484aa4174abc4c852a9f6cd9b542457
Michael Mink <mmink@michaelamink.com>
2020-07-18 14:00:36 -0500

    added arrays to script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+// setup arrays
+var allergyobject = [{ allergy: "Dairy", api: "dairy-free" },
+    { allergy: "Egg", api: "egg-free" },
+    { allergy: "Gluten", api: "gluten-free" },
+    { allergy: "Wheat", api: "wheat-free" },
+    { allergy: "Shellfish", api: "shellfish-free" },
+    { allergy: "Peanuts", api: "peanut-free" },
+    { allergy: "Tree Nuts", api: "tree-nut-free" },
+    { allergy: "Soy", api: "soy-free" }
+];
+
+var mealType = ["Breakfast", "Lunch", "Dinner", "Snack", "Teatime"];
+
+var cuisineType = ["American", "Asian", "Chinese", "French", "Indian", "Italian", "Japanese", "Mexican"];
+
+excludemeat = [{ type: "Fish", api: "fish-free" }, { type: "Pork", api: "pork-free" }, { type: "Red Meat", api: "red-meat-free" }, { type: "fish", api: "fish-free" }];
+excludestartch = ["rice", "corn", "potatoes"];
+excludeveggie = ["brussel sprouts", "zuchinni"];
+excludefruit = ["bannanna", "blueberries"];
