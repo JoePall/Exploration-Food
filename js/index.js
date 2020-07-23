82a526d5bf7f152c810ce01043744a76b6504567
Josiah Powell <josiahpowell@outlook.com>
2020-07-23 13:50:54 -0500

    Lots of changes.

--- js/index.js
+++ js/index.js
@@ -4,47 +4,50 @@
  * @param {string} preferences - the response from the Spoonacular query.
  * TODO: Fill out the rest of the param details.
  */
-function queryAPI(searchText, preferences, callback) {
-    var queryURL = "https://api.spoonacular.com/recipes/complexSearch?query=" + preferences.searchText;
-    var apiKey = "bd8a15cb55ed4be3b746a91ed5d860dd";
-
-    if (!apiKey) {
-        console.log("No API Key");
-        return;
-    }
-
-    if (preferences.cuisine) {
+function queryAPI(preferences, callback) {
+    var queryURL = "https://api.spoonacular.com/recipes/complexSearch?query=" + preferences.Search.replace(" ", "%20");
+    
+    if (preferences.Cuisine && preferences.Cuisine.length > 0) {
         queryURL += "&cuisine=";
-        preferences.cuisine.forEach(item => queryURL += item.replace(" ", "%20"));
+        preferences.Cuisine.forEach(item => queryURL += item.replace(" ", "%20") + ",");
+        queryURL = queryURL.slice(0, -1);
     }
-    if (preferences.diet) {
+    if (preferences.Diet && preferences.Diet.length > 0) {
         queryURL += "&diet=";
-        preferences.diet.forEach(item => queryURL += item.replace(" ", "%20"));
+        preferences.Diet.forEach(item => queryURL += item.replace(" ", "%20") + ",");
+        queryURL = queryURL.slice(0, -1);
     }
-    if (preferences.intolerances) {
+    if (preferences.Intolerances && preferences.Intolerances.length > 0) {
         queryURL += "&intolerances=";
-        preferences.intolerances.forEach(item => queryURL += item.replace(" ", "%20"));
+        preferences.Intolerances.forEach(item => queryURL += item.replace(" ", "%20") + ",");
+        queryURL = queryURL.slice(0, -1);
     }
-    if (preferences.type) {
+    if (preferences.Meal_Type && preferences.Meal_Type.length > 0) {
         queryURL += "&type=";
-        preferences.type.forEach(item => queryURL += item);
+        preferences.Meal_Type.forEach(item => queryURL += item.replace(" ", "%20") + ",");
+        queryURL = queryURL.slice(0, -1);
     }
 
-    queryURL += "&instructionsRequired=true&fillIngredients=true&addRecipeInformation=true&addRecipeNutrition=true&number=1&apiKey=" + apiKey;
+    queryURL += "&instructionsRequired=true&fillIngredients=true&addRecipeInformation=true&addRecipeNutrition=true&number=4&apiKey=" + preferences.apiKey;
     
     console.log(queryURL);
     $.getJSON(queryURL, response => {
+        var result = [];
+        
+        response.results.forEach(item => {
+            var recipe = {
+                title: item.title,
+                source: item.sourceUrl,
+                image: item.image,
+                summary: item.summary,
+                instructions: item.analyzedInstructions,
+                nutrition: item.nutrition,
+                ingredients: item.extendedIngredients
+            };
 
-        var result = {
-            title: response.results[0].title,
-            source: response.results[0].sourceUrl,
-            image: response.results[0].image,
-            summary: response.results[0].summary,
-            instructions: response.results[0].analyzedInstructions,
-            nutrition: response.results[0].nutrition,
-            ingredients: response.results[0].extendedIngredients
-        }
-
+            result.push(recipe);
+        });
+        
         callback(result);
     });
 }
